535c723165c2baeed54a584bfb766b805ba987c5
ponyspy <desade4me@gmail.com>
2018-03-01 21:39:42 +0300

    group

--- apps/praktika/src/js/aa_events.js
+++ apps/praktika/src/js/aa_events.js
@@ -3,7 +3,7 @@ $(function() {
 		e.preventDefault();
 
 
-		$('.group_item').toArray().forEach(function(group, i) {
+		$('.block_item.group').toArray().forEach(function(group, i) {
 			$('<input />').attr('type', 'hidden')
 										.attr('name', 'members' + '[' + i + ']' + '[mode]')
 										.attr('value', $(group).find('.group_mode').val())
@@ -32,26 +32,34 @@ $(function() {
 	});
 
 	$(document)
-		.on('click', '.up_group', function(e) {
-			var $group = $(this).closest('.group_item');
-			$group.insertBefore($group.prev());
+		.on('click', '.up_block', function(e) {
+			var $block = $(this).closest('.block_item');
+			$block.insertBefore($block.prev());
 		})
-		.on('click', '.down_group', function(e) {
-			var $group = $(this).closest('.group_item');
-			if ($group.next().hasClass('add_group')) return false;
-			$group.insertAfter($group.next());
+		.on('click', '.down_block', function(e) {
+			var $block = $(this).closest('.block_item');
+			if ($block.next().hasClass('add_group') || $block.next().hasClass('add_comment')) return false;
+			$block.insertAfter($block.next());
 		})
-		.on('click', '.rm_group', function(e) {
-			if ($('.group_item').size() == 1) return false;
-			$(this).closest('.group_item').remove();
+		.on('click', '.rm_block', function(e) {
+			if ($(this).closest('.block_items').children('.block_item').size() == 1) return false;
+			$(this).closest('.block_item').remove();
+		})
+		.on('click', '.add_comment', function(e) {
+			var $block = $(this).closest('.block_items').children('.block_item');
+			$block.first().clone()
+				.find('option').prop('selected', false).end()
+				.find('textarea').val('').end()
+				.find('input[type=text]').val('').end()
+				.insertAfter($block.last());
 		})
 		.on('click', '.add_group', function(e) {
-			var $group = $(this).closest('.group_members').children('.group_item');
-			$group.first().clone()
+			var $block = $(this).closest('.block_items').children('.block_item');
+			$block.first().clone()
 				.find('.list_item').first().nextAll('.list_item').remove().end().end().end()
 				.find('option').prop('selected', false).end()
 				.find('input[type=text]').val('').end()
-				.insertAfter($group.last());
+				.insertAfter($block.last());
 		})
 		.on('click', '.add_member', function(e) {
 			var $members = $(this).closest('.group_list').children('.list_item');
