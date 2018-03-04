ab04a0e1ce7757f0c3c4b34c72aed7da1b2a22af
ponyspy <desade4me@gmail.com>
2018-03-05 01:21:41 +0300

    contacts

--- apps/praktika/src/js/g_maps.js
+++ apps/praktika/src/js/g_maps.js
@@ -3,10 +3,11 @@ var style_array =
 [{"featureType":"road","stylers":[{"hue":"#5e00ff"},{"saturation":-79}]},{"featureType":"poi","stylers":[{"saturation":-78},{"hue":"#6600ff"},{"lightness":-47},{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"lightness":22}]},{"featureType":"landscape","stylers":[{"hue":"#6600ff"},{"saturation":-11}]},{},{},{"featureType":"water","stylers":[{"saturation":-65},{"hue":"#1900ff"},{"lightness":8}]},{"featureType":"road.local","stylers":[{"weight":1.3},{"lightness":30}]},{"featureType":"transit","stylers":[{"visibility":"simplified"},{"hue":"#5e00ff"},{"saturation":-16}]},{"featureType":"transit.line","stylers":[{"saturation":-72}]},{}]
 
 function initialize() {
+	var map_canvas = document.getElementById('map-canvas');
 	var map_position = new google.maps.LatLng(55.7648875,37.5943699);
 	var marker_position = new google.maps.LatLng(55.765531,37.594230);
 
-	var mapOptions = {
+	var map_options = {
 		zoom: 17,
 		center: map_position,
 		styles: style_array,
@@ -18,17 +19,16 @@ function initialize() {
 		fullscreenControl: false
 	};
 
-	map = new google.maps.Map(document.getElementById('map-canvas'),
-			mapOptions);
+	map = new google.maps.Map(map_canvas, map_options);
 
 	var infowindow = new google.maps.InfoWindow({
-			content: 'театр практика'
+			content: map_canvas.getAttribute('data-placeholder')
 	});
 
 	var marker = new google.maps.Marker({
 			position: marker_position,
 			map: map,
-			title: 'театр практика'
+			title: map_canvas.getAttribute('data-placeholder')
 	});
 
 	google.maps.event.addListener(marker, 'click', function() {
