67ed6d79e22154ebf64b8e4984bb0fed7894a57d
ponyspy <desade4me@gmail.com>
2018-03-09 18:53:55 +0300

    fix

--- apps/praktika/src/js/team.js
+++ apps/praktika/src/js/team.js
@@ -1,9 +1,6 @@
 $(function() {
-	var lorem_text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-	$members = $('.member_item');
-
-	$members.on('click', function(e) {
+	var $members = $('.member_item').on('click', function(e) {
 		var $current_block = $(this);
 		var $set = $current_block.nextAll('.member_item').addBack(this);
 
@@ -12,33 +9,25 @@ $(function() {
 			var $members_list = $('.members_list');
 
 			if ($this.offset().left + $this.width() > $members_list.width() + $members_list.offset().left) {
-				// var $panel = $('<div/>', {'class': 'panel'});
-				// var $in = $('<div/>', {'class': 'in', 'text': $current_block.text() + ' - ' + lorem_text });
-
-				// $('.panel').remove();
-				// $members.removeClass('active');
-
-				// $current_block.addClass('active');
-				// $this.after($panel.append($in));
+				$('.panel').remove();
 
 				$members.removeClass('active');
 				$current_block.addClass('active');
-				$('.panel').show().insertAfter($this);
+
+				$.post('', { id: $current_block.attr('id') }).done(function(content) {
+					$this.after(content);
+				});
 
 				return false;
 			} else if ($this.index('.member_item') + 1 == $members.length) {
-				// var $panel = $('<div/>', {'class': 'panel'});
-				// var $in = $('<div/>', {'class': 'in', 'text': $current_block.text() + ' - ' + lorem_text })
-
-				// $('.panel').remove();
-				// $members.removeClass('active');
-
-				// $current_block.addClass('active');
-				// $set.last().after($panel.append($in));
+				$('.panel').remove();
 
 				$members.removeClass('active');
 				$current_block.addClass('active');
-				$set.last().after($('.panel').show());
+
+				$.post('', { id: $current_block.attr('id') }).done(function(content) {
+					$set.last().after(content);
+				});
 
 				return false;
 			}
