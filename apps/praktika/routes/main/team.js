054a279491a53c0cee1d4ff2acf2683e4b98fd36
ponyspy <desade4me@gmail.com>
2018-03-09 18:39:24 +0300

    init

--- apps/praktika/routes/main/team.js
+++ apps/praktika/routes/main/team.js
@@ -1,14 +1,33 @@
+var jade = require('jade');
+var i18n = require('i18n');
+
 module.exports = function(Model) {
 	var module = {};
 
 	var Member = Model.Member;
+	var Event = Model.Event;
 
 	module.index = function(req, res) {
-		res.render('main/team.jade');
+		Member.find().sort('name.value').exec(function(err, members) {
+			res.render('main/team.jade', { members: members });
+		});
 	};
 
 	module.get_member = function(req, res) {
-		res.send('member!');
+		Member.findOne({ '_short_id': req.body.id }).exec(function(err, member) {
+			Event.find({ 'members.$.list': member._id }).exec(function(err, events) {
+				var opts = {
+					__: function() { return i18n.__.apply(null, arguments); },
+					__n: function() { return i18n.__n.apply(null, arguments); },
+					locale: req.locale,
+					events: events,
+					member: member,
+					compileDebug: false, debug: false, cache: true, pretty: false
+				};
+
+				res.send(jade.renderFile(__app_root + '/views/main/_get_member.jade', opts));
+			});
+		});
 	};
 
 	return module;
