54b04b36a2b1620226544cfd51fe0199a1524e32
ponyspy <desade4me@gmail.com>
2018-03-09 15:16:13 +0300

    members remove

--- apps/praktika/routes/admin/members/remove.js
+++ apps/praktika/routes/admin/members/remove.js
@@ -1,24 +1,34 @@
 var rimraf = require('rimraf');
+var async = require('async');
 
 module.exports = function(Model) {
 	var module = {};
 
 	var Member = Model.Member;
+	var Event = Model.Event;
 
 
-	module.index = function(req, res, next) {
+	module.index = function(req, res) {
 		var id = req.body.id;
 
-		Member.findByIdAndRemove(id).exec(function(err) {
+		async.series([
+			function(callback) {
+				Event.update({'members.list': id}, { $pull: { 'members.$.list': id } }, { 'multi': true }).exec(callback);
+			},
+			function(callback) {
+				Event.update({}, { $pull: { 'members': { 'list': { $size: 0 } } } }, { 'multi': true }).exec(callback);
+			},
+			function(callback) {
+				Member.findByIdAndRemove(id).exec(callback);
+			},
+			function(callback) {
+				rimraf(__glob_root + '/public/cdn/' + __app_name + '/members/' + id, { glob: false }, callback);
+			}
+		], function(err) {
 			if (err) return next(err);
 
-			rimraf(__glob_root + '/public/cdn/' + __app_name + '/members/' + id, { glob: false }, function(err) {
-				if (err) return next(err);
-
-				res.send('ok');
-			});
+			res.send('ok');
 		});
-
 	};
 
 
