28a70dc8c6a47a7485063da812a78a965bbecac1
ponyspy <desade4me@gmail.com>
2018-03-15 14:39:05 +0300

    main swiper

--- apps/praktika/src/js/main.js
+++ apps/praktika/src/js/main.js
@@ -4,10 +4,21 @@ $(function() {
 	};
 
 	var swiper = new Swiper('.swiper-container', {
+		init: false,
+		effect: 'fade',
 		initialSlide: randomInteger(0, $('.slide_item').length),
 		pagination: {
 			clickable: true,
 			el: '.swiper-pagination',
-		}
+		},
+		runCallbacksOnInit: true
 	});
+
+	swiper.on('init slideChange', function(e) {
+		var style = $('.slide_item').eq(swiper.activeIndex).attr('data-style');
+
+		$('.maket_block').removeClass('white black').addClass(style);
+	});
+
+	swiper.init();
 });
