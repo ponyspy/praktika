a3c2b0dc451dddb51c7696888e84c91917c3128c
ponyspy <desade4me@gmail.com>
2018-03-31 13:15:04 +0300

    fade

--- apps/praktika/src/js/event.js
+++ apps/praktika/src/js/event.js
@@ -1,6 +1,7 @@
 $(function() {
 	var swiper = new Swiper('.swiper-container', {
 		loop: true,
+		effect: 'fade',
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
