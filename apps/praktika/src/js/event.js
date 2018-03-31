122f75f5fc7928e0a13fd6500d281790f41786b2
ponyspy <desade4me@gmail.com>
2018-03-31 19:37:27 +0300

    schedule

--- apps/praktika/src/js/event.js
+++ apps/praktika/src/js/event.js
@@ -26,12 +26,21 @@ $(function() {
 		swiper.slideNext();
 	});
 
+	if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		$('.event_schedule').on('mousemove', function(e) {
+			var $this = $(this).children('.schedule_outer');
+
+			var percent = (e.pageX - $this.offset().left) / $this.width() * 1.1 - 0.25;
+			$this.scrollLeft($this.children('.schedule_inner').width() * percent);
+		});
+	}
+
 	$(document)
 		.on('scroll', function(e) {
 			var $head = $('.event_head');
 			var $schedule = $('.event_schedule');
 
-			$(this).scrollTop() >= $head.height() + $head.offset().top - $schedule.height() - 30
+			$(this).scrollTop() >= $head.height() + $head.offset().top - $schedule.height()
 				? $schedule.addClass('fix')
 				: $schedule.removeClass('fix');
 		})
