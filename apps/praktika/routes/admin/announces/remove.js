6adc7f6a075ddeab3317c6a41fc9156b771c8b8b
ponyspy <desade4me@gmail.com>
2018-05-23 22:29:04 +0300

    remove

--- apps/praktika/routes/admin/announces/remove.js
+++ apps/praktika/routes/admin/announces/remove.js
@@ -4,19 +4,23 @@ module.exports = function(Model) {
 	var module = {};
 
 	var Announce = Model.Announce;
+	var Media = Model.Media;
 
 
 	module.index = function(req, res, next) {
 		var id = req.body.id;
 
-
 		Announce.findByIdAndRemove(id).exec(function(err) {
 			if (err) return next(err);
 
-			rimraf(__glob_root + '/public/cdn/' + __app_name + '/announces/' + id, { glob: false }, function(err) {
+			Media.update({ announce: id }, { $unset: { announce: 1 } }).exec(function(err) {
 				if (err) return next(err);
 
-				res.send('ok');
+				rimraf(__glob_root + '/public/cdn/' + __app_name + '/announces/' + id, { glob: false }, function(err) {
+					if (err) return next(err);
+
+					res.send('ok');
+				});
 			});
 		});
 	};
--- apps/praktika/routes/main/index.js
+++ apps/praktika/routes/main/index.js
@@ -1,10 +1,17 @@
+var moment = require('moment');
+
 module.exports = function(Model) {
 	var module = {};
 
 	var Media = Model.Media;
 
 	module.index = function(req, res) {
-		Media.find().sort('date').where('status').ne('hidden').populate('announce').exec(function(err, medias) {
+		Media.find().sort('date').where('status').ne('hidden').populate({
+			path: 'announce',
+			match: { 'status': { $ne: 'hidden' },
+							 'interval.start': { $gte: moment().toDate() },
+							 'interval.end': { $lte: moment().toDate() },
+			}}).exec(function(err, medias) {
 
 			var banner = req.cookies.banner
 				? +req.cookies.banner <= 2
