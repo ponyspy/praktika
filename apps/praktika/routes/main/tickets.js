ccc9aa61785654f6d8fe411da667654ff591b680
ponyspy <desade4me@gmail.com>
2018-09-14 12:45:57 +0300

    fix

--- apps/praktika/routes/main/tickets.js
+++ apps/praktika/routes/main/tickets.js
@@ -21,13 +21,13 @@ module.exports = function() {
 			},
 			timeout: 6000,
 			json: true
-		}
+		};
 
 		request.get(options, function(err, resp, body) {
 			if (err || body.message) return res.send('err');
 
 			var out = body.map(function(event) {
-				return {'show_id': event.show_id,  'date': moment(event.show_start).format('DD.MM.YYYY HH:mm')}
+				return {'show_id': event.show_id,  'date': moment(event.show_start).format('DD.MM.YYYY HH:mm')};
 			});
 
 			res.send(out);
@@ -45,7 +45,7 @@ module.exports = function() {
 			},
 			timeout: 6000,
 			json: true
-		}
+		};
 
 		request.get(options, function(err, resp, body) {
 			if (err || body.message) return res.send('err');
@@ -54,7 +54,7 @@ module.exports = function() {
 				var event_date = moment(event.show_start);
 
 				if (event_date.isBetween(req.body.min, req.body.max, 'minutes', '[]')) {
-					arr.push(event_date.format('DD.MM.YYYY HH:mm'));
+					arr.push({'show_id': event.show_id, 'date': event_date.format('DD.MM.YYYY HH:mm')});
 				}
 
 				return arr;
@@ -70,7 +70,7 @@ module.exports = function() {
 		var query = req.query;
 		var uri = req.app.locals.static_types.intickets_widget_uri;
 
-		res.redirect(uri + '/' + query.show_id)
+		res.redirect(uri + '/' + query.show_id);
 	};
 
 	return module;
