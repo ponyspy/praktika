8a99d5cd9fcf101e142e04295baf08ce581921e0
ponyspy <desade4me@gmail.com>
2019-01-30 15:58:13 +0300

    status

--- apps/praktika/routes/main/partners.js
+++ apps/praktika/routes/main/partners.js
@@ -4,7 +4,7 @@ module.exports = function(Model) {
 	var Partner = Model.Partner;
 
 	module.index = function(req, res) {
-		Partner.find().where('status').ne('hidden').sort('-date').exec(function(err, partners) {
+		Partner.find().where('status').nin(['hidden', 'special']).sort('-date').exec(function(err, partners) {
 			res.render('main/partners.pug', { partners: partners });
 		});
 	};
