9c69160e5658d32d10b89f01d4222d630c0683cc
ponyspy <desade4me@gmail.com>
2022-07-28 13:18:35 +0300

    google api key

--- apps/praktika/routes/main/contacts.js
+++ apps/praktika/routes/main/contacts.js
@@ -2,7 +2,7 @@ module.exports = function(Model) {
 	var module = {};
 
 	module.index = function(req, res, next) {
-		res.render('main/contacts.pug');
+		res.render('main/contacts.pug', {'static_types':req.app.locals.static_types});
 	};
 
 	return module;
