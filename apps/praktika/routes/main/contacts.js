41f5d9ca9ceb993b681363f3aebf182bd1319139
ponyspy <desade4me@gmail.com>
2022-07-28 13:28:13 +0300

    fix

--- apps/praktika/routes/main/contacts.js
+++ apps/praktika/routes/main/contacts.js
@@ -2,7 +2,7 @@ module.exports = function(Model) {
 	var module = {};
 
 	module.index = function(req, res, next) {
-		res.render('main/contacts.pug', {'static_types':req.app.locals.static_types});
+		res.render('main/contacts.pug', {'static_keys':req.app.locals.static_keys});
 	};
 
 	return module;
