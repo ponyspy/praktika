e5b09705a5457575d8a47a97e91ac418b79f91d8
ponyspy <desade4me@gmail.com>
2024-11-05 23:15:57 +0300

    tickets error

--- apps/praktika/routes/main/tickets.js
+++ apps/praktika/routes/main/tickets.js
@@ -27,8 +27,7 @@ module.exports = function() {
 		};
 
 		request.get(options, function(err, resp, body) {
-			if (err || body.message) return res.send('err');
-			console.log(body)
+			if (body || err || body.message) return res.send('err');
 
 			var out = body.map(function(event) {
 				return {'soldout': event.sold_out, 'cost': event.cost, 'show_id': event.show_id,  'date': moment(event.show_start).format('DD.MM.YYYY HH:mm')};
@@ -51,7 +50,7 @@ module.exports = function() {
 		};
 
 		request.get(options, function(err, resp, body) {
-			if (err || body.message) return res.send('err');
+			if (body || err || body.message) return res.send('err');
 
 			var out = body.reduce(function(arr, event) {
 				var event_date = moment(event.show_start);
